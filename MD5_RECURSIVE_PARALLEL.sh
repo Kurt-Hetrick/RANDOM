811ee1b64e1c3e8103f7fcf88e29bc98bd4c9382
Kurt Hetrick <khetric1@jhmi.edu>
2018-03-15 11:00:48 -0400

    changed using gnu parallel from isilon path to module load. the percentage of cpu procs used is now configurable if a 2nd argument is used, but 75% is the default if you don't specify a 2nd argument. the output name is now md5_target-directory_timestamp now instead of md_release_timestamp

--- MD5_RECURSIVE_PARALLEL.sh
+++ MD5_RECURSIVE_PARALLEL.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-RELEASE_DIR=$1
+DIRECTORY_PATH=$1
+PCT_CPU_PROC_TO_USE=$2
 
-GNU_PARALLEL_DIR="/isilon/sequencing/Kurt/Programs/PATH/parallel"
+if [[ -z "$PCT_CPU_PROC_TO_USE" ]]
+	then
+	PCT_CPU_PROC_TO_USE=75
+fi
+
+module load parallel/20161222
 
 TIMESTAMP=`date '+%F.%H-%M-%S'`
 
-find $RELEASE_DIR -type f | cut -f 2 | parallel --no-notice --eta --jobs 75% md5sum {} \
-> $RELEASE_DIR/"md5_release_"$TIMESTAMP".txt"
+LAST_FOLDER=`basename $DIRECTORY_PATH`
+
+find $DIRECTORY_PATH -type f \
+| cut -f 2 \
+| parallel --no-notice --eta --jobs $PCT_CPU_PROC_TO_USE"%" md5sum {} \
+> $DIRECTORY_PATH/"md5_"$LAST_FOLDER"_"$TIMESTAMP".txt"
