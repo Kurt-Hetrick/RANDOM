6739a67b1c140412e9416760a67fa99b5ef35081
Kurt Hetrick <khetric1@jhmi.edu>
2023-09-11 15:54:51 -0400

    added header with qsub parameters to SELECT_SAMPLES_FROM_MS_VCF.sh so that users can qsub this with having to supply there own parameters.

--- SELECT_SAMPLES_FROM_MS_VCF.sh
+++ SELECT_SAMPLES_FROM_MS_VCF.sh
@@ -1,43 +1,70 @@
 #!/bin/bash
 
+# ---qsub parameter settings---
+# --these can be overrode at qsub invocation--
+
+# tell sge to execute in bash
+#$ -S /bin/bash
+
+# tell sge that you are in the users current working directory
+#$ -cwd
+
+# tell sge to export the users environment variables
+#$ -V
+
+# use these queues
+#$ -q prod.q,rnd.q,bigdata.q
+
+# tell sge to submit at this priority setting
+#$ -p -4
+
+# tell sge to output both stderr and stdout to the same file
+#$ -j y
+
+# write log file to
+#$ -o /mnt/research/tools/LINUX/00_GIT_REPO_KURT/LOGS
+
 set
 
 echo
 
-# INPUT ARGUMENTS
+echo this is a test of the emergency broadcast system
 
-	SAMPLE_LIST=$1 #file should have a .args extension. should only contain the sample id. 1 per row
-	IN_VCF=$2 # input vcf. full path
-	OUT_VCF=$3 # output vcf file name prefix.
-	REF_GENOME=$4 # optional. if there is no 4th argument present then grch37 is used.
 
-		if [[ ! ${REF_GENOME} ]]
-			then
-			REF_GENOME="/mnt/research/tools/PIPELINE_FILES/bwa_mem_0.7.5a_ref/human_g1k_v37_decoy.fasta"
-		fi
+# # INPUT ARGUMENTS
 
-# PROGRAM DEPENDENCIES
+# 	SAMPLE_LIST=$1 #file should have a .args extension. should only contain the sample id. 1 per row
+# 	IN_VCF=$2 # input vcf. full path
+# 	OUT_VCF=$3 # output vcf file name prefix.
+# 	REF_GENOME=$4 # optional. if there is no 4th argument present then grch37 is used.
 
-	JAVA_1_8="/mnt/research/tools/LINUX/JAVA/jdk1.8.0_73/bin"
-	GATK_DIR_4011="/mnt/linuxtools/GATK/gatk-4.0.11.0"
+# 		if [[ ! ${REF_GENOME} ]]
+# 			then
+# 			REF_GENOME="/mnt/research/tools/PIPELINE_FILES/bwa_mem_0.7.5a_ref/human_g1k_v37_decoy.fasta"
+# 		fi
 
-# output file will be written to the same directory that the input file is located at.
+# # PROGRAM DEPENDENCIES
+
+# 	JAVA_1_8="/mnt/research/tools/LINUX/JAVA/jdk1.8.0_73/bin"
+# 	GATK_DIR_4011="/mnt/linuxtools/GATK/gatk-4.0.11.0"
+
+# # output file will be written to the same directory that the input file is located at.
 	
-	OUT_DIR=$(dirname $IN_VCF)
+# 	OUT_DIR=$(dirname $IN_VCF)
 
-# Extract out sample
+# # Extract out sample
 
-	CMD=$JAVA_1_8'/java -jar'
-	CMD=${CMD}" -Dsamjdk.compression_level=6 -jar"
-	CMD=$CMD' '$GATK_DIR_4011'/gatk-package-4.0.11.0-local.jar'
-	CMD=$CMD' SelectVariants'
-	CMD=$CMD' --reference '$REF_GENOME
-	CMD=$CMD' --variant '$IN_VCF
-	CMD=$CMD' --output '$OUT_DIR'/'$OUT_VCF'.vcf.gz'
-	CMD=$CMD' --exclude-non-variants'
-	CMD=$CMD' --remove-unused-alternates'
-	CMD=$CMD' --sample-name '$SAMPLE_LIST
+# 	CMD=$JAVA_1_8'/java -jar'
+# 	CMD=${CMD}" -Dsamjdk.compression_level=6 -jar"
+# 	CMD=$CMD' '$GATK_DIR_4011'/gatk-package-4.0.11.0-local.jar'
+# 	CMD=$CMD' SelectVariants'
+# 	CMD=$CMD' --reference '$REF_GENOME
+# 	CMD=$CMD' --variant '$IN_VCF
+# 	CMD=$CMD' --output '$OUT_DIR'/'$OUT_VCF'.vcf.gz'
+# 	CMD=$CMD' --exclude-non-variants'
+# 	CMD=$CMD' --remove-unused-alternates'
+# 	CMD=$CMD' --sample-name '$SAMPLE_LIST
 
-echo $CMD | bash
+# echo $CMD | bash
 
-echo $OUT_VCF".vcf.gz" was written to $OUT_DIR
+# echo $OUT_VCF".vcf.gz" was written to $OUT_DIR
